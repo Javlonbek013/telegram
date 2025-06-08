f07d9625677978e9c8129a71a05e89761a64ce40
javlonbek013 <nomonov@gmail.com>
2025-06-08 14:34:25 +0500

    asd

--- server.js
+++ server.js
@@ -65,3 +65,10 @@ app.post('/send-message', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server ${PORT} portda ishlayapti`);
 });
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
